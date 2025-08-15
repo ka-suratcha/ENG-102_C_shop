5f864c86b8a9829e243dd5d53929ea060c889e70
ka-suratcha <ka.suratcha@gmail.com>
2025-08-15 22:49:03 +0700

    fix tax caculating

--- main.c
+++ main.c
@@ -17,14 +17,16 @@ int main()
     // price list
     // 5 categories, 10 items each
     float price[5][11] = {
-        {1.0f, 3.15f, 8.60f, 1.63f, 1.06f, 5.30f, 1.38f, 7.0f, 1.16f, 1.46f, 1.0f},
+        {1.0f, 1.0f, 8.60f, 1.63f, 1.06f, 5.30f, 1.38f, 7.0f, 1.16f, 1.46f, 3.15f},
         {2.0f, 1.11f, 1.68f, 0.75f, 1.00f, 5.5f, 2.19f, 7.5f, 19.92f, 5.24f, 8.78f},
         {3.0f, 3.84f, 2.50f, 2.39, 4.98, 3.46f, 5.97f, 1.97f, 1.97f, 4.97f, 1.47f},
         {4.0f, 1.97f, 1.97f, 2.97f, 7.97f, 2.98f, 1.17f, 1.97f, 3.67f, 4.97f, 1.67f},
         {5.0f, 3.97f, 3.77f, 3.47f, 7.98f, 6.01f, 2.00f, 3.00f, 4.78f, 2.78f, 3.97f}};
 
     // Variables
-    float pay = 0, change = 0, total = 0.0f;
+    float pay = 0, change = 0;
+    float total = 0.0f, total_with_tax = 0.0f;
+    float tax_rate = 0.08f;
 
     // Shopping list
     // 1000 items, each with 3 attributes: category, item, quantity
@@ -140,20 +142,27 @@ int main()
         system("cls");
 
         // Calculate total price
-        total += price[ansType - 1][ansItem] * amount;
-        printf("\n++++++++++++++++++++++++++++++++++++++++++++++++\n");
+        total += price[ansType - 1][ansItem] * (float)amount;
+        total_with_tax = total * (1.0f + tax_rate);
+
+        // Add to shopping list
         shopList[listCurrent][0] = ansType - 1;
         shopList[listCurrent][1] = ansItem;
         shopList[listCurrent][2] = amount;
         listCurrent++;
-        printf("Added %s to the shopping list (+$%.2f). \nYour total is now $%.2f (+$%.2f Tax)", grocery[ansType - 1][ansItem], price[ansType - 1][ansItem] * amount, total * 1.08, total * 0.08f);
+
+        printf("\n++++++++++++++++++++++++++++++++++++++++++++++++\n");
+        printf("Added %s to the shopping list. \nYour total is now $%.2f (+$%.2f Tax)",
+               grocery[ansType - 1][ansItem],
+               total_with_tax,
+               total * tax_rate);
         printf("\n++++++++++++++++++++++++++++++++++++++++++++++++\n");
 
         // printf shoplist
         printf("\nYour purchases: \n");
         for (i = 1; i <= listCurrent; i++)
         {
-            printf("[%d] %s ($%.2f)  x%d\n", i, grocery[shopList[i - 1][0]][shopList[i - 1][1]], price[shopList[i - 1][0]][shopList[i - 1][1]] * amount, amount);
+            printf("[%d] %s ($%.2f)  x%d\n", i, grocery[shopList[i - 1][0]][shopList[i - 1][1]], price[shopList[i - 1][0]][shopList[i - 1][1]] * (float)amount * (1.0f + tax_rate), amount);
         }
 
         // Buy more?
@@ -186,7 +195,9 @@ int main()
     bool passp = true;
     while (passp)
     {
-        printf("\nYour total is $%.2f (+$%.2f Tax).", total * 1.08, total * 0.08f);
+        printf("\nYour total is $%.2f (+$%.2f Tax).", 
+            total * (1.0f + tax_rate), 
+            total * tax_rate);
         printf("\nYour money : ");
         scanf("%f", &pay);
         if (pay < total)
@@ -206,9 +217,9 @@ int main()
     // printf shoplist
     for (i = 1; i <= listCurrent; i++)
     {
-        printf("[%d] %s ($%.2f)  x%d\n", i, grocery[shopList[i - 1][0]][shopList[i - 1][1]], price[shopList[i - 1][0]][shopList[i - 1][1]] * amount, amount);
+        printf("[%d] %s ($%.2f)  x%d\n", i, grocery[shopList[i - 1][0]][shopList[i - 1][1]], price[shopList[i - 1][0]][shopList[i - 1][1]] * (float)amount, amount);
     }
-    printf("\nThank you for our purchase! \nYour total is $%.2f (+$%.2f Tax). \nYour Change is $%.2f .", total * 1.08, total * 0.08f, change);
+    printf("\nThank you for our purchase! \nYour total is $%.2f (+$%.2f Tax). \nYour Change is $%.2f .", total * (1.0f + tax_rate), total * (1.0f + tax_rate), change);
 
     return 0;
 }
